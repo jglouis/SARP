75fe1d2c3a51a31d1ff7fcff8bf29b1f051cb4b4
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2015-12-21 16:44:21 +0100

    print node labels.

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
+try:
+    import matplotlib.pyplot as plt
+except:
+    raise
+
 import networkx as nx
-import matplotlib.pyplot as plt
 
-graph = nx.DiGraph()
-#graph.add_nodes_from([2,3])
-graph.add_weighted_edges_from([
-        (1,2,1),(2,3,4),
-        (1,4,2), (2,5,5),(3,6,7),
-        (4,5,11),(5,6,12),
-        (4,7,9),(5,8,16),(6,9,14),
-        (7,8,15),(8,9,18)])
+G=nx.Graph()
 
+G.add_edge(1,2,weight=1)
+G.add_edge(2,1,weight=3)
+G.add_edge(2,3,weight=4)
+G.add_edge(3,2,weight=6)
+G.add_edge(1,4,weight=2)
+G.add_edge(4,1,weight=8)
+G.add_edge(2,5,weight=5)
+G.add_edge(5,2,weight=10)
+G.add_edge(3,6,weight=7)
+G.add_edge(6,3,weight=13)
+G.add_edge(5,4,weight=11)
+G.add_edge(5,5,weight=21)
+G.add_edge(5,6,weight=12)
 
-nx.draw(graph)
+elarge=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] >0.5]
+esmall=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] <=0.5]
 
+pos=nx.spring_layout(G) # positions for all nodes
+
+# nodes
+nx.draw_networkx_nodes(G,pos,node_size=700)
+
+# edges
+nx.draw_networkx_edges(G,pos,edgelist=elarge,
+                    width=6)
+nx.draw_networkx_edges(G,pos,edgelist=esmall,
+                    width=6,alpha=0.5,edge_color='b',style='dashed')
+
+# labels
+nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
+
+plt.axis('off')
+plt.savefig("multigraph.png") # save as png
 plt.show()
